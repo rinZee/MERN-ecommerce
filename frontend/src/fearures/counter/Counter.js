8811f665c04406a2d4846d23efbb24e320c45419
unknown <trex33160@gmail.com>
2021-10-03 16:57:20 +0545

    react setup

---
+++ frontend/src/fearures/counter/Counter.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./counterSlice";
+
+export function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        ></button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        ></button>
+      </div>
+    </div>
+  );
+}
