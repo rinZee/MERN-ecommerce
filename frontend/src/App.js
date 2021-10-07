251dcb23c6e6b5356cc2f2a07346229206c91bc3
unknown <trex33160@gmail.com>
2021-10-07 10:49:39 +0545

    added some products to homescreen, rating, styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import "./App.css";
-import { Counter } from "./fearures/counter/Counter";
+import { Container } from "react-bootstrap";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
-    <div className="App">
-      <Counter />
-    </div>
+    <>
+      <Header />
+      <main>
+        <Container className="py-3">
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
