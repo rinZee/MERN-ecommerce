24880aef1d0493716847cd6154a938cb26e354b0
unknown <trex33160@gmail.com>
2021-10-10 21:43:58 +0545

    created a redux store and connected with redux-devtools-extension

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./fearures/counter/counterSlice";
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({});
+
+const initialState = {};
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
