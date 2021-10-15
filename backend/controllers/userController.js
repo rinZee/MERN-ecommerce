b93deccb5019beef6b5a460baaac54f4b4189e09
unknown <trex33160@gmail.com>
2021-10-15 23:46:19 +0545

    added user registration and password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import asyncHandler from "express-async-handler";
 
 import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
+
 // @desc autheticate user and get token
 // @route POST /api/users/login
 // @access Public
@@ -24,6 +25,38 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Register a new user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invaid user data");
+  }
+});
+
 // @desc get user profile
 // @route GET /api/users/profile
 // @access Public
@@ -42,4 +75,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error("Invalid email or password");
   }
 });
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
